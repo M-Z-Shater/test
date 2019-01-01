29b508ed69ba6d80441fe3f7ea83c6b2c564e842
unknown <mohamed.zakria.shater@gmail.com>
2019-01-02 01:09:51 +0200

    a commit

--- Try.kt
+++ Try.kt
@@ -1,4 +1,6 @@
 fun main(Array<String> Args){
     print("this is first commit")
     print("this is second commit")
+//a68df5f4eb9b60812edaad6176ccaf48e20c69bf
+    //aaf422606606cac694ae4dca173d88317b89c7d6
 }
